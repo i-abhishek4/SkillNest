ba03bd9f44df07295301494239473b9af378a200
i-abhishek4 <abhishekalli47@gmail.com>
2025-05-11 14:15:29 +0530

    Designed schemas

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-efre
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+const Freelancer=require("./models/Freelancer");
+
+mongoose.connect("mongodb://localhost:27017/skillnest")
+.then(()=>{console.log("Connection established successfully")})
+.catch((err)=>{console.log("Mongodb connection error",err)});
+
+
+app.listen(3000,()=>{
+    console.log("Server running");
+})
+
